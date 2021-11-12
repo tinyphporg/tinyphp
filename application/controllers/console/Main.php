28d1488085e4e72bfadf0611b0c7314ee2d9832f
tinycn <tinycn@qq.com>
2021-11-12 19:19:54 +0800

    update devs

--- application/controllers/console/Main.php
+++ application/controllers/console/Main.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controller\Console;
 
+use Tiny\Data\Db\DbException;
+
 class Main extends \Tiny\MVC\Controller\ConsoleController
 {
     public function onstart()
@@ -24,9 +26,25 @@ class Main extends \Tiny\MVC\Controller\ConsoleController
     
     public function indexAction()
     {
-        echo $this->config['status.index'];
-        echo "aaaa";
-        return;
+        
+        $actionName = $this->request->get['a'];
+        $controllerName = $this->request->get['c'];
+        $name = $this->request->get['name'];
+        
+        //$isName = $this->request->get->isRequired('name') ? 'true' : 'false';
+        // 模型使用
+        $userInfo = $this->mainUserInfoModel->getUsers();
+        $this->assign([
+            'actionName' => $actionName,
+            'controllerName' => $controllerName,
+            'name' => $name,
+            'defName' => 'tinyphp',
+            'isName' => $isName,
+            'users' => $userInfo,
+            'users1' => $userInfo1
+        ]);
+        echo "aa1";
+
     }
 
 }
