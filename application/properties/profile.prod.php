d77e91c86a4fbb3cd48941f05e6c6271ddb17749
king <king@LAPTOP-6J27AVDS.mshome.net>
2023-04-04 20:11:00 +0800

    rebuild

--- application/properties/profile.prod.php
+++ application/properties/profile.prod.php
@@ -1,3 +1,21 @@
 <?php 
-return [];
+$profile = [];
+$profile['debug']['enabled'] = false;
+/**
+ * Application引导类
+ *
+ * 通过监听引导事件触发
+ *
+ * bootstrap.enabled
+ *      开启引导
+ *
+ * bootstrap.event_listener
+ *      string 实现Bootstrapevent_listener的类名
+ *      array [实现Bootstrapevent_listener的类名]
+ *
+ */
+$profile['bootstrap']['enabled'] = true;
+$profile['bootstrap']['event_listener'] = \App\Event\Bootstrap::class;
+
+return $profile;
 ?>
